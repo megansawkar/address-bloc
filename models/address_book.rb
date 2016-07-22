c35b4383f7f06c9048ff8039c19abbe709644b57
Megan Sawkar <mwelch122@gmail.com>
2016-07-22 11:40:14 -0700

    Assignment 22 Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -30,22 +30,36 @@ class AddressBook
     end
   end
 
-  def binary_search(name)
-    lower = 0
-    upper = entries.length - 1
+  #def binary_search(name)
+  #  lower = 0
+  #  upper = entries.length - 1
 
-    while lower <= upper
-      mid = (lower + upper) / 2
-      mid_name = entries[mid].name
+  #  while lower <= upper
+  #    mid = (lower + upper) / 2
+  #    mid_name = entries[mid].name
 
-      if name == mid_name
-        return entries[mid]
-      elsif name < mid_name
-        upper = mid - 1
-      elsif name > mid_name
-        lower = mid + 1
+  #    if name == mid_name
+  #      return entries[mid]
+  #    elsif name < mid_name
+  #      upper = mid - 1
+  #    elsif name > mid_name
+  #      lower = mid + 1
+  #    end
+  #  end
+  #  return nil
+  #end
+
+  def iterative_search(name)
+    index = 0
+    while index <= (entries.length - 1)
+      if name == entries[index].name
+        return entries[index]
+      else
+        index += 1
       end
     end
     return nil
   end
+
+
 end
